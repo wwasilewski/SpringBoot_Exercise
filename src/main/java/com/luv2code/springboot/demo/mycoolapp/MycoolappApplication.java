6353caa4a0248b37d3a0f24bea3ca5d515dded55
wwasilewski <w.wasilewski@gmail.com>
2019-07-09 16:24:23 +0200

    adding simple rest controller to show Hello World message with LocalDateTime

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MycoolappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MycoolappApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MycoolappApplication.class, args);
+    }
 }
---
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,20 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Wojciech Wasilewski
+ * @date Created on 09.07.2019
+ */
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World, time on the server is: " + LocalDateTime.now();
+    }
+}
