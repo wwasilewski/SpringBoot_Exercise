acb237a6a42f36207fdf839d2931789ddcaa4e1b
wwasilewski <w.wasilewski@gmail.com>
2019-07-10 13:00:20 +0200

    adding student.name, team.name in application.properties and injecting them in FunRestController, adding new /teaminfo endpoint

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +14,32 @@ import java.time.LocalDateTime;
 @RestController
 public class FunRestController {
 
+    // inject properties for student.name and team.name
+    @Value("${student.name}")
+    private String studentName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Student: " + studentName + ", Team: " + teamName;
+    }
+
+    // expose "/" with Hello World and time
     @GetMapping("/")
     public String sayHello() {
         return "Hello World, time on the server is: " + LocalDateTime.now();
     }
 
+    // expose "/workout"
     @GetMapping("/workout")
     public String getDailyWorkout() {
         return "Run 5km";
     }
 
+    // expose "hobby"
     @GetMapping("/hobby")
     public String getWork() {
         return "Time to play some tennis";
